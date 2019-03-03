fa990a946327b8895fb6b79130749225004dc1f0
dookadoo <35851914+dookadoo@users.noreply.github.com>
2019-03-03 09:41:37 -0700

    a

--- server.js
+++ server.js
@@ -66,7 +66,9 @@ router.post('/signup', function(req, res) {
             password: req.body.password
         };
         // save the user
-        db.save(newUser); //no duplicate checking
+        if(db.contains(newUser){
+			res.json({success: false, msg: 'User already Exists.'});
+		}else{db.save(newUser);}
         res.json({success: true, msg: 'Successful created new user.'});
     }
 });
