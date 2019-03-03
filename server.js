d46cd77280f3dd4cb24a86e1a3f14e164c899726
dookadoo <35851914+dookadoo@users.noreply.github.com>
2019-03-03 11:05:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ router.route('/postjwt')
 
 router.post('/signup', function(req, res) {
     if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: 'Please pass username and password.', req: req, res: res});
+        res.json({success: false, msg: 'Please pass username and password.', UNIQUE_KEY: process.env.UNIQUE_KEY});
     } else {
         var newUser = {
             username: req.body.username,
@@ -68,9 +68,9 @@ router.post('/signup', function(req, res) {
         };
         // save the user
         if(db.contains(newUser)){
-			res.json({success: false, msg: 'User already Exists.'});
+			res.json({success: false, msg: 'User already Exists.', UNIQUE_KEY: process.env.UNIQUE_KEY});
 		}else{db.save(newUser);}
-        res.json({success: true, msg: 'Successful created new user.'});
+        res.json({success: true, msg: 'Successful created new user.', UNIQUE_KEY: process.env.UNIQUE_KEY});
     }
 });
 
@@ -79,7 +79,7 @@ router.post('/signin', function(req, res) {
         var user = db.findOne(req.body.username);
 
         if (!user) {
-            res.status(401).send({success: false, msg: 'Authentication failed. User not found.'});
+            res.status(401).send({success: false, msg: 'Authentication failed. User not found.', UNIQUE_KEY: process.env.UNIQUE_KEY});
         }
         else {
             // check if password matches
@@ -89,7 +89,7 @@ router.post('/signin', function(req, res) {
                 res.json({success: true, token: 'JWT ' + token});
             }
             else {
-                res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
+                res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'}, UNIQUE_KEY: process.env.UNIQUE_KEY);
             }
         };
 });
