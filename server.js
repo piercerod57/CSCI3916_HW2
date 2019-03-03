d5aaf57bb779314d79d56689a7369d7e23bd700f
dookadoo <35851914+dookadoo@users.noreply.github.com>
2019-03-03 15:37:17 -0700

    afdfdf

--- server.js
+++ server.js
@@ -59,9 +59,13 @@ router.route('/postjwt')
     );
 
 router.post('/signup', function(req, res) {
-	let responseHeader = req.body;
-    if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: 'Please pass username and password.', UNIQUE_KEY: process.env.UNIQUE_KEY});
+	if(req.body != null){
+		let responseHeader = req.body;//@NOTE(P): sends back query parameters
+    }else{let responseHeader = null;}
+	let responseHeader = req.headers;//@NOTE(P): sends back query headers
+	
+	if (!req.body.username || !req.body.password) {
+        res.json({success: false, msg: 'Please pass username and password.', headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
     } else {
         var newUser = {
             username: req.body.username,
@@ -69,18 +73,21 @@ router.post('/signup', function(req, res) {
         };
         // save the user
         //if(db.contains(newUser)){
-			res.json({success: false, msg: 'User already Exists.', UNIQUE_KEY: process.env.UNIQUE_KEY});
+			res.json({success: false, msg: 'User already Exists.', headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
 		/*}else{*/db.save(newUser);//}
-        res.json({success: true, msg: 'Successful created new user.', UNIQUE_KEY: process.env.UNIQUE_KEY, query: responseHeader});
+        res.json({success: true, msg: 'Successful created new user.', headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
     }
 });
 
 router.post('/signin', function(req, res) {
-		let responseHeader = req.body;
+		if(req.body != null){
+			let responseBody = req.body;//@NOTE(P): sends back query parameters
+		}else{let responseBody = null;}
+		let responseHeader = req.headers;//@NOTE(P): sends back query headers
         var user = db.findOne(req.body.username);
 
         if (!user) {
-            res.status(401).send({success: false, msg: 'Authentication failed. User not found.', UNIQUE_KEY: process.env.UNIQUE_KEY, query: responseHeader});
+            res.status(401).send({success: false, msg: 'Authentication failed. User not found.', headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
         }
         else {
             // check if password matches
@@ -90,10 +97,136 @@ router.post('/signin', function(req, res) {
                 res.json({success: true, token: 'JWT ' + token, query: req.query});
             }
             else {
-                res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.', UNIQUE_KEY: process.env.UNIQUE_KEY, query: responseHeader});
+                res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.', headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
             }
         };
 });
+
+/*
+HTTP Method: GET should return { 
+status: 200, 
+message: ‘GET movies”,  
+headers: header from request,
+query: query string from request,
+env: your unique key
+*/
+router.get('/movies', function(req, res) {
+		if(req.body != null){
+			let responseHeader = req.body;//@NOTE(P): sends back query parameters
+		}else{let responseHeader = null;}
+		let responseHeader = req.headers;//@NOTE(P): sends back query headers
+        
+		var movie = db.find(reg.body.movieid);
+		if(!movie){
+			res.status(401).send({success: false, msg: "movie not found", headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
+		}else{
+			res.json({msg: "movie found", headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY, user: movie});
+		}
+});
+
+/*
+ “status”:  200,  
+ message:  “movie saved”, 
+ headers: headers: header from request,
+query: query string 
+env: your unique key
+}*/
+router.post('/movies', function(req, res) {
+		if(req.body != null){
+			let responseHeader = req.body;//@NOTE(P): sends back query parameters
+		}else{let responseHeader = null;}
+		let responseHeader = req.headers;//@NOTE(P): sends back query headers
+        //@TODO(P): find function
+		var movie = db.find(reg.body.movieid);
+		if(!movie){
+			res.status(401).send({success: false, msg: "movie already exists", headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
+		}else{
+			
+			
+			res.json({msg: "Created", headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
+		}
+});
+
+
+/*
+status:  200,  message:  “movie updated”
+headers: header from request,
+query: query string from request, 
+env: your unique key
+*/
+router.put('/movies', function(req, res) {//requires jwtauth
+		if(req.body != null){
+			let responseHeader = req.body;//@NOTE(P): sends back query parameters
+		}else{let responseHeader = null;}
+		let responseHeader = req.headers;//@NOTE(P): sends back query headers
+		var movie = db.find(reg.body.movieid);
+		var user = db.findOne(req.body.username);
+
+		if (!user) {
+            res.status(401).send({success: false, msg: 'Authentication failed. User not found.', headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
+        }
+        else {
+            // check if password matches
+            if (req.body.password == user.password)  {
+				//@NOTE(P): update movie db
+                var userToken = { id : user.id, username: user.username };
+                var token = jwt.sign(userToken, process.env.SECRET_KEY);
+				
+					if (req.body.password == user.password)  {
+						if(!movie){
+							res.status(401).send({success: false, msg: "Error: Can't find movie", token: 'JWT ' + token, headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
+						}else{
+							var result = db.update(movie, user);
+							if(result === 1){
+								res.json({msg: "movie updated", token: 'JWT ' + token, headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
+							}else if(result === 0){
+								res.status(401).send({success: false, msg: "Error: movie not updated", token: 'JWT ' + token, headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
+							}
+						}
+					}
+			}else {
+                res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.', headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
+            }
+        };
+});
+
+
+/*
+“status:  200,  
+message:  “movie deleted”
+headers: header from request,
+query: query string from request, 
+env: your unique key
+*/
+router.delete('/movies', function(req, res) {//requires auth
+		if(req.body != null){
+			let responseHeader = req.body;//@NOTE(P): sends back query parameters
+		}else{let responseHeader = null;}
+		let responseHeader = req.headers;//@NOTE(P): sends back query headers
+        var user = db.findOne(req.body.username);
+		var movie = db.find(reg.body.movieid);
+		
+		if (!user) {
+            res.status(401).send({success: false, msg: 'Authentication failed. User not found.', headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
+        }
+        else {
+            // check if password matches
+            if (req.body.password == user.password)  {
+				//@NOTE(P): Remove movie from db
+				if(!movie){
+					res.status(401).send({success: false, msg: "Error: Can't find movie", headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
+				}else{
+					var found = db.remove(movie);
+					res.json({msg: "movie deleted", headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY, found: found});
+				}
+            }
+            else {
+                res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.', headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
+            }
+        };
+});
+
+
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
