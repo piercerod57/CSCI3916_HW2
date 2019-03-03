0af182c3cbebcdf24634698a5c08f4bb2fc776c0
dookadoo <35851914+dookadoo@users.noreply.github.com>
2019-03-03 16:16:38 -0700

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ router.post('/signup', function(req, res) {
 			res.json({success: false, msg: 'User already Exists.', headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY});
 		}else{*/
 			db.save(newUser);
-			res.json({success: true, msg: 'Successful created new user.', headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY, test: testVar});
+			res.json({success: true, msg: 'Successful created new user.', headers: responseHeader, query: responseBody, env: process.env.UNIQUE_KEY, test: test});
 		//}
     }
 });
